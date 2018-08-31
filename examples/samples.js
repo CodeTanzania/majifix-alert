6e27b4c84c96e8e0827091c786a9b97868d74280
lykmapipo <lallyelias87@gmail.com>
2018-08-31 11:28:50 +0300

    improve samples to include statistics

--- examples/samples.js
+++ examples/samples.js
@@ -8,6 +8,13 @@ function sample() {
   return {
     subject: faker.hacker.noun(),
     message: faker.lorem.sentence(),
+    statistics: {
+      SMS: {
+        sent: faker.random.number(5555, 9999),
+        delivered: faker.random.number(3333, 4444),
+        failed: faker.random.number(1111, 2222),
+      }
+    }
   };
 }
 
