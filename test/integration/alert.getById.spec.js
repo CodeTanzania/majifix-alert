79421fd4a64ce7ab1d0b49777c54bd6c4f15f6fe
rijkerd <richardaggrey7@gmail.com>
2019-06-07 19:42:51 +0300

    chore(integration): implement to es6

--- test/integration/alert.getById.spec.js
+++ test/integration/alert.getById.spec.js
@@ -1,106 +1,76 @@
-'use strict';
-
 /* dependencies */
-const path = require('path');
-const _ = require('lodash');
-const { expect } = require('chai');
-const { Jurisdiction } = require('@codetanzania/majifix-jurisdiction');
-const { Alert } = require(path.join(__dirname, '..', '..'));
+import _ from 'lodash';
+import { expect } from 'chai';
+import { Jurisdiction } from '@codetanzania/majifix-jurisdiction';
+import { clear, create } from '@lykmapipo/mongoose-test-helpers';
+import { Alert } from '../../src/index';
 
 describe('Alert', () => {
+  const jurisdiction = Jurisdiction.fake();
 
-  let jurisdiction;
+  before(done => clear(Alert, Jurisdiction, done));
 
-  before(done => {
-    Jurisdiction.deleteMany(done);
-  });
-
-  before(done => {
-    jurisdiction = Jurisdiction.fake();
-    jurisdiction.post((error, created) => {
-      jurisdiction = created;
-      done(error, created);
-    });
-  });
-
-  before(done => {
-    Alert.deleteMany(done);
-  });
+  before(done => create(jurisdiction, done));
 
   describe('get by id', () => {
-
     let alert;
 
     before(done => {
       alert = Alert.fake();
       alert.jurisdictions = [].concat(jurisdiction);
-      alert
-        .post((error, created) => {
-          alert = created;
-          done(error, created);
-        });
+      alert.post((error, created) => {
+        alert = created;
+        done(error, created);
+      });
     });
 
     it('should be able to get an instance', done => {
+      Alert.getById(alert._id, (error, found) => {
+        expect(error).to.not.exist;
+        expect(found).to.exist;
+        expect(found._id).to.eql(alert._id);
 
-      Alert
-        .getById(alert._id, (error, found) => {
-          expect(error).to.not.exist;
-          expect(found).to.exist;
-          expect(found._id).to.eql(alert._id);
-
-          //assert jurisdiction
-          expect(found.jurisdictions).to.exist;
-          done(error, found);
-        });
-
+        // assert jurisdiction
+        expect(found.jurisdictions).to.exist;
+        done(error, found);
+      });
     });
 
     it('should be able to get with options', done => {
-
       const options = {
         _id: alert._id,
-        select: 'subject'
+        select: 'subject',
       };
 
-      Alert
-        .getById(options, (error, found) => {
-          expect(error).to.not.exist;
-          expect(found).to.exist;
-          expect(found._id).to.eql(alert._id);
-          expect(found.subject).to.exist;
+      Alert.getById(options, (error, found) => {
+        expect(error).to.not.exist;
+        expect(found).to.exist;
+        expect(found._id).to.eql(alert._id);
+        expect(found.subject).to.exist;
 
-          //...assert selection
-          const fields = _.keys(found.toObject());
-          expect(fields).to.have.length(3);
-          _.map([
-            'message'
-          ], field => {
-            expect(fields).to.not.include(field);
-          });
-
-
-          done(error, found);
+        // ...assert selection
+        const fields = _.keys(found.toObject());
+        expect(fields).to.have.length(3);
+        _.map(['message'], field => {
+          expect(fields).to.not.include(field);
         });
 
+        done(error, found);
+      });
     });
 
     it('should throw if not exists', done => {
-
-      const alert = Alert.fake();
+      alert = Alert.fake();
       alert.jurisdictions = [].concat(jurisdiction);
 
-      Alert
-        .getById(alert._id, (error, found) => {
-          expect(error).to.exist;
-          // expect(error.status).to.exist;
-          expect(error.name).to.be.equal('DocumentNotFoundError');
-          expect(found).to.not.exist;
-          done();
-        });
-
+      Alert.getById(alert._id, (error, found) => {
+        expect(error).to.exist;
+        // expect(error.status).to.exist;
+        expect(error.name).to.be.equal('DocumentNotFoundError');
+        expect(found).to.not.exist;
+        done();
+      });
     });
-
   });
 
   after(done => {
@@ -110,5 +80,4 @@ describe('Alert', () => {
   after(done => {
     Jurisdiction.deleteMany(done);
   });
-
 });
--- test/integration/alert.post.spec.js
+++ test/integration/alert.post.spec.js
@@ -1,85 +1,53 @@
-'use strict';
-
 /* dependencies */
-const path = require('path');
-const { expect } = require('chai');
-const { Jurisdiction } = require('@codetanzania/majifix-jurisdiction');
-const { Alert } = require(path.join(__dirname, '..', '..'));
+import { expect } from 'chai';
+import { Jurisdiction } from '@codetanzania/majifix-jurisdiction';
+import { clear, create } from '@lykmapipo/mongoose-test-helpers';
+import { Alert } from '../../src/index';
 
 describe('Alert', () => {
+  const jurisdiction = Jurisdiction.fake();
 
-  let jurisdiction;
+  before(done => clear(Alert, Jurisdiction, done));
 
-  before(done => {
-    Jurisdiction.deleteMany(done);
-  });
-
-  before(done => {
-    jurisdiction = Jurisdiction.fake();
-    jurisdiction.post((error, created) => {
-      jurisdiction = created;
-      done(error, created);
-    });
-  });
-
-  before(done => {
-    Alert.deleteMany(done);
-  });
+  before(done => create(jurisdiction, done));
 
   describe('static post', () => {
-
     let alert;
 
     it('should be able to post', done => {
-
       alert = Alert.fake();
       alert.jurisdictions = [].concat(jurisdiction);
 
-      Alert
-        .post(alert, (error, created) => {
-          expect(error).to.not.exist;
-          expect(created).to.exist;
-          expect(created._id).to.eql(alert._id);
-          expect(created.subject).to.equal(alert.subject);
+      Alert.post(alert, (error, created) => {
+        expect(error).to.not.exist;
+        expect(created).to.exist;
+        expect(created._id).to.eql(alert._id);
+        expect(created.subject).to.equal(alert.subject);
 
-          //assert jurisdiction
-          expect(created.jurisdictions).to.exist;
+        // assert jurisdiction
+        expect(created.jurisdictions).to.exist;
 
-          done(error, created);
-        });
-
+        done(error, created);
+      });
     });
-
   });
 
   describe('instance post', () => {
-
     let alert;
 
     it('should be able to post', done => {
-
       alert = Alert.fake();
       alert.jurisdictions = [].concat(jurisdiction);
 
-      alert
-        .post((error, created) => {
-          expect(error).to.not.exist;
-          expect(created).to.exist;
-          expect(created._id).to.eql(alert._id);
-          expect(created.subject).to.equal(alert.subject);
-          done(error, created);
-        });
-
+      alert.post((error, created) => {
+        expect(error).to.not.exist;
+        expect(created).to.exist;
+        expect(created._id).to.eql(alert._id);
+        expect(created.subject).to.equal(alert.subject);
+        done(error, created);
+      });
     });
-
   });
 
-  after(done => {
-    Alert.deleteMany(done);
-  });
-
-  after(done => {
-    Jurisdiction.deleteMany(done);
-  });
-
+  after(done => clear('Alert', 'Jurisdiction', done));
 });
